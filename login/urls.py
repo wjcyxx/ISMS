e1f580be6a870d9dc2e08b26bc4b89c169ab6336
macpc <11325818@qq.com>
2019-07-07 22:11:16 +0800

    修改login模块,增加前台view

--- login/urls.py
+++ login/urls.py
@@ -6,5 +6,6 @@ app_name="login"
 urlpatterns = [
     path('index/', views.index, name='login'),
     path('login_chk/', views.login_chk, name='login_chk'),
+    path('login_showPrj', views.login_showPrj, name='login_showPrj'),
     # path('login_ok/', views.login_ok, name='login_ok'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,32 +1,25 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from .models import *
 import json
-
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
-
-
 def login_chk(request):
     if request.method == "POST":
-        uact = request.POST.get("email")
-        upwd = request.POST.get("password")
-        id = request.GET.get("id")
+        UserID = request.POST.get("UserID")
+        Userpwd = request.POST.get("Userpwd")
 
         response_data = {}
         try:
-            if id == '0':
-                user_info = user.objects.get(Q(FEmail=uact), Q(FType=0))
-            elif id == '1':
-                user_info = user.objects.get(Q(FMobtel=uact), Q(FType=1))
-            elif id == '2':
-                user_info = user.objects.get(Q(FUseraccount=uact), Q(FType=3))
+            user_info = User.objects.get(Q(FUserID=UserID))
 
-            if upwd != user_info.FPwd:
+            if Userpwd != user_info.FUserpwd:
                 response_data['result'] = '1'  # 返回密码输入错误
                 return HttpResponse(json.dumps(response_data))
         except ObjectDoesNotExist:
@@ -35,6 +28,14 @@ def login_chk(request):
             return HttpResponse(json.dumps(response_data))
 
         response_data['result'] = '0'  # 返回正常登录
-        request.session['uact'] = uact
-        request.session['utag'] = user_info.FUseraccount
+        response_data['orgid'] = user_info.FOrgID
+
+        request.session['UserID'] = UserID
+        request.session['Username'] = user_info.FUsername
+        request.session['UserOrg'] = user_info.FOrgID
         return HttpResponse(json.dumps(response_data))
+
+def login_showPrj(request):
+
+    if request.method == "GET":
+        return render(request, "content/login/showproject.html")
