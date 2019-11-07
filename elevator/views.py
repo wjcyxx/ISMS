42ac301f6936b1ebcd34448a76a7861d4703983d
weijing <11325818@qq.com>
2019-11-07 09:15:06 +0800

    进行升降机模块

--- elevator/views.py
+++ elevator/views.py
@@ -13,6 +13,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from baseframe.baseframe import *
 from project.models import project
 from personnel.models import personnel
+from menchanical.models import menchanical
 # Create your views here.
 
 #控制器入口
@@ -22,9 +23,13 @@ class entrance(EntranceView_base):
         project_info = project.objects.get(Q(FID=prj_id))
         person_info = personnel.objects.filter(Q(FStatus=0), Q(CREATED_PRJ=prj_id), Q(FSpecialequ=True))
 
+        safemanager_info = menchanical.objects.filter(Q(FStatus=True), Q(CREATED_PRJ=prj_id), Q(FMectypeID='fa606fec009311eaab497831c1d24216')).values('FMecmanager', 'FMecmanagertel').distinct()
+
+
         self.template_name = 'content/elevator/elevatorvisual.html'
         self.context['project_info'] = project_info
         self.context['person_info'] = person_info
+        self.context['safemanager_info'] = safemanager_info
 
 
 class get_datasource(View):
@@ -37,20 +42,57 @@ class get_datasource(View):
         initID = '7293af48cfab11e9b5c17831c1d24216'
         result = get_interface_result(initID, [token])['data']
 
+        initSubID = 'b49d3f2ed04d11e9b9dd7831c1d24216'
+
         resultdict = []
 
         for dt in result:
             dict = {}
 
             dict['hoist_box_id'] = dt['hoist_box_id']
-
+            result_run = get_interface_result(initSubID, [token, dt['hoist_box_id']])['data']
+            if len(result_run) == 0:
+                dict['status'] = 0
+                dict['cage_id'] = '--'
+                dict['door_lock_state'] = '--'
+                dict['driver_identification_state'] = '--'
+                dict['height_percentage'] = '--'
+                dict['hoist_system_state'] = '--'
+                dict['hoist_time'] = '--'
+                dict['real_time_gradient1'] = '--'
+                dict['real_time_gradient2'] = '--'
+                dict['real_time_height'] = '--'
+                dict['real_time_lifting_weight'] = 0
+                dict['real_time_number_of_people'] = '--'
+                dict['real_time_or_alarm'] = '--'
+                dict['real_time_speed'] = '--'
+                dict['real_time_speed_direction'] = 3
+                dict['tilt_percentage1'] = '--'
+                dict['tilt_percentage2'] = '--'
+                dict['weight_percentage'] = '--'
+            else:
+                dict['status'] = 1
+                dict['cage_id'] = result_run[0]['cage_id']
+                dict['door_lock_state'] = result_run[0]['door_lock_state']
+                dict['driver_identification_state'] = result_run[0]['driver_identification_state']
+                dict['height_percentage'] = result_run[0]['height_percentage']
+                dict['hoist_system_state'] = result_run[0]['hoist_system_state']
+                dict['hoist_time'] = result_run[0]['hoist_time']
+                dict['real_time_gradient1'] = result_run[0]['real_time_gradient1']
+                dict['real_time_gradient2'] = result_run[0]['real_time_gradient2']
+                dict['real_time_height'] = result_run[0]['real_time_height']
+                dict['real_time_lifting_weight'] = result_run[0]['real_time_lifting_weight']
+                dict['real_time_number_of_people'] = result_run[0]['real_time_number_of_people']
+                dict['real_time_or_alarm'] = result_run[0]['real_time_or_alarm']
+                dict['real_time_speed'] = result_run[0]['real_time_speed']
+                dict['real_time_speed_direction'] = result_run[0]['real_time_speed_direction']
+                dict['tilt_percentage1'] = result_run[0]['tilt_percentage1']
+                dict['tilt_percentage2'] = result_run[0]['tilt_percentage2']
+                dict['weight_percentage'] = result_run[0]['weight_percentage']
 
             resultdict.append(dict)
 
-
-
-
-        #return JsonResponse(resultdict, safe=False)
+        return HttpResponse(json.dumps(resultdict))
 
 
 class get_run_datasource(View):
--- menchanical/models.py
+++ menchanical/models.py
@@ -52,6 +52,7 @@ class mecoperauth(models.Model):
     FAuthTimeslot = models.CharField(max_length=32, verbose_name='授权操作时间段', blank=True, null=True)
     FAuthDeadline = models.CharField(max_length=32, verbose_name='截止日期', blank=True, null=True)
     FDesc = models.CharField(max_length=1024, verbose_name='备注', blank=True, null=True)
+    FStatus = models.BooleanField(default=True, verbose_name='状态')
     CREATED_PRJ = models.CharField(max_length=32, verbose_name='所属项目', blank=True, null=True)
     CREATED_ORG = models.CharField(max_length=32, verbose_name='创建组织', blank=True, null=True)
     CREATED_BY = models.CharField(max_length=32, verbose_name='创建人', blank=True, null=True)
